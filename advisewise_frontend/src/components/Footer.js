90a9efa9c5f3a2506d307b4bb930caac576dd49d
Joaquin Uriarte <juriartebruno@gmail.com>
2023-06-19 19:52:49 -0400

    changed slogan!

--- advisewise_frontend/src/components/Footer.js
+++ advisewise_frontend/src/components/Footer.js
@@ -2,7 +2,7 @@ function Footer() {
     return (
         <footer className="header-footer-main">
             <div className="flex flex-col items-center">
-                <h1 className="header-footer-text">Education Personalized for You</h1>
+                <h1 className="header-footer-text">Personalize Your Education</h1>
             </div>
         </footer>
     );
