7f5ea1860e68dc471f8c0864c2d5653bd02c5ec3
Joaquin Uriarte <juriartebruno@gmail.com>
2023-07-15 16:56:35 -0400

    Refractor Pivot
    Half way through changing four year plan functionaility
    About to refractor code so that index.js is parent of main, header, and Footer

--- advisewise_frontend/src/lib/backend.js
+++ advisewise_frontend/src/lib/backend.js
@@ -1,6 +1,21 @@
+export async function fetchAllFourYearPlans(student_id) {
+  try {
+    const response = await fetch(`http://localhost:4000/schedule/allPlans/${student_id}`);
+    if (!response.ok) {
+      console.error('fetchAllFourYearPlans: Failed to fetch data from the backend');
+      return []; // Return an empty array if the response is not ok
+    }
+    const classes = await response.json();
+    return classes || []; // Ensure classes is an array even if it's undefined
+  } catch (error) {
+    console.error('An error occurred while fetching the data:', error);
+    return []; // Return an empty array in case of an error
+  }
+}
+
 export async function fetchAllClasses() {
   try {
-    const response = await fetch('http://localhost:4000/schedule/all');
+    const response = await fetch('http://localhost:4000/schedule/allCourses');
     if (!response.ok) {
       console.error('Failed to fetch data from the backend');
       return []; // Return an empty array if the response is not ok
--- advisewise_frontend/src/pages/index.js
+++ advisewise_frontend/src/pages/index.js
@@ -1,30 +1,41 @@
 import ClassTable from "@/components/classTable/classTable";
 import FourYearPlan from "@/components/fourYearPlan/fourYearPlan";
 import ChatBox from "@/components/chatbox/ChatBox";
-import { fetchAllClasses, fetchAllSemesters, fetchAllSemesterClasses, updateFourYearPlanAdd, updateFourYearPlanRemove } from '../lib/backend.js';
+import { fetchAllFourYearPlans, fetchAllClasses, fetchAllSemesters, fetchAllSemesterClasses, updateFourYearPlanAdd, updateFourYearPlanRemove } from '../lib/backend.js';
 import { useState, useEffect } from "react";
 
-export default function Home({ classes, semesters, all_semester_classes }) {
+export default function Home({ startingPlan, fourYearPlans, classes, semesters, all_semester_classes }) {
   
   const [semClasses, setSemClasses] = useState([]);
   const [selectedSemester, setSelectedSemester] = useState(null);
+  const [currentPlan, setCurrentPlan] = useState(startingPlan);
+  
+  //Test: make this work
+  console.log(fourYearPlans);
 
+  const updateSemestersAndClasses = (semesterList, allSemClasses) => {
+    let currentSemClasses = [];
 
-  useEffect(() => {
-    let initialSemClasses = [];
-
-    semesters.forEach((semester) => {
+    semesterList.forEach((semester) => {
       const semesterId = semester.id;
-      const semesterClasses = all_semester_classes[semesterId] || [];
-      initialSemClasses.push([semesterId, semesterClasses]);
+      const semesterClasses = allSemClasses[semesterId] || [];
+      currentSemClasses.push([semesterId, semesterClasses]);
     });
 
-    setSemClasses(initialSemClasses);
-    
+    setSemClasses(currentSemClasses);
+  };
+
+  useEffect(() => {
+    updateSemestersAndClasses(semesters, all_semester_classes);
   }, [all_semester_classes, semesters]);
 
   const handleSemesterSelection = (semesterId) => {
     setSelectedSemester(semesterId);
+  };
+
+  const handleFourYearPlanSelection = (fourYearPlanId) => {
+    setCurrentPlan(fourYearPlanId);
+    handleChangeFourYearPlan(fourYearPlanId);
   };
 
   const eliminateClassFromSemester = async (selectedClass) => {
@@ -61,7 +72,6 @@ export default function Home({ classes, semesters, all_semester_classes }) {
     }
   };
   
-
   const addClassToSemester = async (selectedClass) => {
     if (selectedSemester === null) {
       // No semester selected
@@ -93,6 +103,25 @@ export default function Home({ classes, semesters, all_semester_classes }) {
       console.log("Failed to add class");
     }
   };
+
+  const handleChangeFourYearPlan = async (newPlanId) => {
+    // Fetch semesters for the new plan
+    const newSemesters = await fetchAllSemesters({ fourYearPlan: newPlanId });
+
+    // Fetch classes for each semester of the new plan
+    const all_semester_classes = {};
+    for (let i = 0; i < semesters.length; i++) {
+      try {
+        all_semester_classes[semesters[i].id] = await fetchAllSemesterClasses(semesters[i].id);
+      } catch (error) {
+        console.error('Error fetching semester classes:', error);
+      }
+    }
+  
+    // Update semClasses with the new semesters
+    updateSemestersAndClasses(newSemesters, all_semester_classes);
+    
+  }
   
 
   return (
@@ -119,11 +148,35 @@ export default function Home({ classes, semesters, all_semester_classes }) {
 
 export async function getStaticProps() {
   try {
-    const [classes, semesters] = await Promise.all([
+    // SECTION 1: FETCHING DATA
+    // Fetch four-year plans and classes data concurrently using Promise.all for better performance.
+    const [fourYearPlans, classes] = await Promise.all([
+      fetchAllFourYearPlans(1),
       fetchAllClasses(),
-      fetchAllSemesters({ fourYearPlan: 1 }),
     ]);
+
+    // SECTION 2: DETERMINING THE OFFICIAL PLAN
+    // Start with the assumption that the first plan in the array is the official plan.
+    let officialPlanId = fourYearPlans[0]['id']; 
+
+    // Iterate over the four-year plans to find if there is a plan marked as 'official'.
+    for (let i = 0; i < fourYearPlans.length; i++) {
+      if (fourYearPlans[i]['official'] === true) {
+        // If an 'official' plan is found, update officialPlanId and break out of the loop.
+        officialPlanId = fourYearPlans[i]['id']; 
+        break; 
+      }
+    }
+
+    // SECTION 3: FETCHING SEMESTERS
+    // Fetch semesters associated with the determined official plan.
+    const semesters = await fetchAllSemesters({ fourYearPlan: officialPlanId }); 
+
+    // SECTION 4: FETCHING SEMESTER CLASSES
+    // Prepare a container to hold the fetched semester classes.
     const all_semester_classes = {};
+
+    // Iterate over the fetched semesters to fetch classes for each semester.
     for (let i = 0; i < semesters.length; i++) {
       try {
         all_semester_classes[semesters[i].id] = await fetchAllSemesterClasses(semesters[i].id);
@@ -131,15 +184,22 @@ export async function getStaticProps() {
         console.error('Error fetching semester classes:', error);
       }
     }
+
+    // SECTION 5: RETURNING PROPS
+    // Return all fetched data as props for the component.
     return {
       props: {
+        startingPlan: officialPlanId, 
+        fourYearPlans: fourYearPlans || [], 
         classes: classes || [],
         semesters: semesters || [],
         all_semester_classes: all_semester_classes,
       },
     };
   } catch (error) {
+    // In case of any error during the fetching process, log the error and re-throw to handle it upstream.
     console.error('Error fetching classes and semesters:', error);
     throw error;
   }
 }
+
